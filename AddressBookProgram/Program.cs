6f1731098e5feaca7a32b9c4b6d64be0562292e6
Snehal979 <bansodsnehal535@gmail.com>
2023-02-04 00:04:55 +0530

    [Snehal]Add: created the contact class with variable/Propeerty

--- AddressBookProgram/Program.cs
+++ AddressBookProgram/Program.cs
@@ -1,11 +1,24 @@
-﻿namespace AddressBookProgram
+﻿using System.Net;
+
+namespace AddressBookProgram
 {
     class Program
     {
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcom To Address Book Program");
-            Console.ReadLine();
+            Contact contact = new Contact()   //Object creation
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Zip = Convert.ToInt64(Console.ReadLine()),
+                PhoneNUmber = Console.ReadLine(),
+                Email = Console.ReadLine()
+            };
+           
         }
     }
 }
