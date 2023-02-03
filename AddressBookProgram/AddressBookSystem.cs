ee2acfb06801e5ceffb0be510b19b655db52f132
Snehal979 <bansodsnehal535@gmail.com>
2023-02-04 00:21:10 +0530

    [Snehal]Add:Create the Contact One Person

---
+++ AddressBookProgram/AddressBookSystem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProgram
+{
+    public class AddressBookSystem
+    {
+        Contact contact = new Contact();
+        /// <summary>
+        /// Uc2 Create Contact
+        /// </summary>
+        public void CreateContact()
+        {
+            Console.WriteLine("Enter the First Name");
+            contact.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter the Last Name");
+            contact.LastName = Console.ReadLine();
+            Console.WriteLine("Enter the Address");
+            contact.Address = Console.ReadLine();
+            Console.WriteLine("Enter the City");
+            contact.City = Console.ReadLine();
+            Console.WriteLine("Enter the State ");
+            contact.State = Console.ReadLine();
+            Console.WriteLine("Enter the Zip");
+            contact.Zip = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter the PhoneNumber");
+            contact.PhoneNUmber = Console.ReadLine();
+            Console.WriteLine("Enter the Email");
+            contact.Email = Console.ReadLine();
+
+        }
+        /// <summary>
+        /// Display Method
+        /// </summary>
+        public void Display()
+        {
+            Console.WriteLine("Contact Details:" + "\n" + "FirstName: " + contact.FirstName +
+                   "\n" + "LastName: " + contact.LastName + "\n" + "Address: " + contact.Address + "\n" + "City: " + contact.City + "\n"
+                   + "State: " + contact.State + "\n" + "Zip: " + contact.Zip + "\n" + "PhoneNumber: " + contact.PhoneNUmber + "\n" + "Email: " + contact.Email);
+        }
+    }
+}
--- AddressBookProgram/Program.cs
+++ AddressBookProgram/Program.cs
@@ -7,18 +7,10 @@ namespace AddressBookProgram
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcom To Address Book Program");
-            Contact contact = new Contact()   //Object creation
-            {
-                FirstName = Console.ReadLine(),
-                LastName = Console.ReadLine(),
-                Address = Console.ReadLine(),
-                City = Console.ReadLine(),
-                State = Console.ReadLine(),
-                Zip = Convert.ToInt64(Console.ReadLine()),
-                PhoneNUmber = Console.ReadLine(),
-                Email = Console.ReadLine()
-            };
-           
+            AddressBookSystem bookSystem = new AddressBookSystem();
+            bookSystem.CreateContact();
+            bookSystem.Display();
+            Console.ReadLine();
         }
     }
 }
