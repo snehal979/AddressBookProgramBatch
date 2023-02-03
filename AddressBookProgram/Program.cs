5dec3afe9efc0c78cec713c1d8862a7f730db709
Snehal979 <bansodsnehal535@gmail.com>
2023-02-03 23:53:51 +0530

    [Snehal]Add: Welcome message

---
+++ AddressBookProgram/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AddressBookProgram
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcom To Address Book Program");
+            Console.ReadLine();
+        }
+    }
+}
